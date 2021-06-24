dfae18bcd788c942265ce1aaecaa405adb3d58c5
David Walters <littledew13099@gmail.com>
2021-06-24 16:04:17 -0400

    attemp a reconnect

--- networking/events/handlers/WELCOME.js
+++ networking/events/handlers/WELCOME.js
@@ -20,35 +20,47 @@ module.exports = class Welcome extends BaseEvent {
     this._bot.on._emit(this._command, data);
 
     if (!data.loggedInUser) {
-      const loginSettings = this._bot.config._loginSettings;
-      const result = await this._websocket.emit(request.SECURITY_LOGIN, {
-        headers: {
-          version: 2
-        },
-        body: {
-          type: loginSettings.loginType,
-          deviceTypeId: toDeviceTypeId(loginSettings.loginDevice),
-          onlineState: loginSettings.onlineState,
-          username: loginSettings.email,
-          password: crypto.createHash('md5').update(loginSettings.password).digest('hex'),
-          md5Password: true
+      const login = async (bot) => {
+        const loginSettings = bot.config._loginSettings;
+
+        const result = await this._websocket.emit(request.SECURITY_LOGIN, {
+          headers: {
+            version: 2
+          },
+          body: {
+            type: loginSettings.loginType,
+            deviceTypeId: toDeviceTypeId(loginSettings.loginDevice),
+            onlineState: loginSettings.onlineState,
+            username: loginSettings.email,
+            password: crypto.createHash('md5').update(loginSettings.password).digest('hex'),
+            md5Password: true
+          }
+        });
+        if (!result.success) {
+          bot.on._emit(internal.LOGIN_FAILED, result);
+
+          if (result.headers && result.headers.subCode && result.headers.subCode > 1) {
+            await bot.utility().delay(90000);// Attempt to reconnect after 30 seconds regardless of expiry given
+            await login(bot);
+          }
+
+          return;
         }
-      });
 
-      if (!result.success) {
-        this._bot.on._emit(internal.LOGIN_FAILED, result);
-        return;
-      }
+        bot.on._emit(internal.LOGIN_SUCCESS, result.body.subscriber);
+        bot._cognito = result.body.cognito;
+        bot.currentSubscriber = result.body.subscriber;
+        bot._endpointConfig = data.endpointConfig;
 
-      this._bot.on._emit(internal.LOGIN_SUCCESS, result.body.subscriber);
-      this._bot._cognito = result.body.cognito;
-      this._bot.currentSubscriber = result.body.subscriber;
-      this._bot._endpointConfig = data.endpointConfig;
+        this.onSuccess(false);
+      };
+
+      return await login(this._bot);
     } else {
       this._bot.currentSubscriber = data.loggedInUser;
-    }
 
-    this.onSuccess(data.loggedInUser);
+      this.onSuccess(true);
+    }
   }
 
   async onSuccess (reconnect = false) {
