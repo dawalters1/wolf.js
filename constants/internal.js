d19d16022b192012033ec0ec3b433376f06fe94d
Dave <littledew13099@gmail.com>
2021-08-18 20:57:50 -0400

    5th rewrite?

--- constants/internal.js
+++ constants/internal.js
@@ -1,28 +1,55 @@
 
 module.exports = {
-  CONNECTING: 'connecting',
   CONNECTED: 'connect',
-  DISCONNECTED: 'disconnect',
-  RECONNECTING: 'reconnecting',
-  RECONNECTED: 'reconnected',
+  CONNECTING: 'connecting',
   CONNECTION_ERROR: 'connect_error',
   CONNECTION_TIMEOUT: 'connect_timeout',
-  GROUP_MESSAGE: 'group message',
-  NOTIFICATION_RECEIVED: 'notification received',
-  PING: 'ping',
-  PONG: 'pong',
-  PRIVATE_MESSAGE: 'private message',
-  PACKET_SENT: 'packet sent',
-  PACKET_RECEIVED: 'packet received',
-  LOGIN_SUCCESS: 'login success',
-  LOGIN_FAILED: 'login failed',
-  LOG: 'log',
+  DISCONNECTED: 'disconnect',
+  ERROR: 'error',
+
   INTERNAL_ERROR: 'internal error',
+
   JOINED_GROUP: 'joined group',
   LEFT_GROUP: 'left group',
-  READY: 'ready',
+
+  LOG: 'log',
+
+  LOGIN_FAILED: 'login failed',
+  LOGIN_SUCCESS: 'login success',
+
+  GROUP_MESSAGE: 'group message',
+  PRIVATE_MESSAGE: 'private message',
+
+  NOTIFICATION_RECEIVED: 'notification received',
+
+  PACKET_RECEIVED: 'packet received',
+  PACKET_SENT: 'packet sent',
+
   PERMISSIONS_FAILED: 'permissions failed',
+
+  PING: 'ping',
+  PONG: 'pong',
+
   PRIVATE_MESSAGE_ACCEPT_RESPONSE: 'private message accept response',
+
+  READY: 'ready',
+
+  RECONNECTED: 'reconnected',
+  RECONNECTING: 'reconnecting',
   RECONNECT_FAILED: 'reconnect failed',
-  ERROR: 'error'
+
+  STAGE_CLIENT_BROADCAST_ENDED: 'stage client broadcast ended',
+  STAGE_CLIENT_CONNECTING: 'stage client connecting',
+  STAGE_CLIENT_CONNECTED: 'stage client connected',
+  STAGE_CLIENT_DISCONNECTED: 'stage client disconnected',
+  STAGE_CLIENT_DURATION_UPDATE: 'stage client duration',
+  STAGE_CLIENT_ERROR: 'stage client error',
+  STAGE_CLIENT_KICKED: 'stage client kicked',
+  STAGE_CLIENT_MUTED: 'stage client muted',
+  STAGE_CLIENT_PAUSED: 'stage client paused',
+  STAGE_CLIENT_READY: 'stage client ready',
+  STAGE_CLIENT_STOPPED: 'stage client stopped',
+  STAGE_CLIENT_UNMUTED: 'stage client unmuted',
+  STAGE_CLIENT_UNPAUSED: 'stage client unpaused',
+  STAGE_CLIENT_VIEWER_COUNT_CHANGED: 'stage client viewer count changed'
 };
