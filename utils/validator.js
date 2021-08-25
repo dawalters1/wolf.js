663391fef6c09b93f994b5b83004d2fd2505abbe
Dave <littledew13099@gmail.com>
2021-08-25 16:28:26 -0400

    merge validator package

---
+++ utils/validator.js
@@ -0,0 +1,29 @@
+const isNull = arg => arg === null;
+
+const isNullOrWhitespace = arg => (arg === null || arg === undefined) || (typeof (arg) === 'string' && arg.trim().length === 0);
+
+const isLessThanOrEqualZero = arg => !(parseInt(arg) > 0);
+
+const isLessThanZero = arg => !(parseInt(arg) >= 0);
+
+const isValidNumber = arg => !Number.isNaN(arg);
+
+const isValidArray = arg => Array.isArray(arg);
+
+const isValidBoolean = arg => typeof arg === 'boolean' || (typeof arg === 'number' && (arg === 1 || arg === 0));
+
+const isValidDate = arg => !isNaN(new Date(arg).getDate());
+
+const isBuffer = arg => Buffer.isBuffer(arg);
+
+module.exports = {
+  isNull,
+  isLessThanOrEqualZero,
+  isNullOrWhitespace,
+  isValidNumber,
+  isValidArray,
+  isLessThanZero,
+  isValidBoolean,
+  isValidDate,
+  isBuffer
+};
