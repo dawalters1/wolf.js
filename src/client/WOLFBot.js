1026e566af9d91953da63580ad246687af30b98a
Dave <littledew13099@gmail.com>
2021-11-13 13:31:12 -0500

    typings

--- src/client/WOLFBot.js
+++ src/client/WOLFBot.js
@@ -36,50 +36,8 @@ const Subscriber = require('../helper/subscriber/Subscriber');
 const Tipping = require('../helper/tipping/Tipping');
 
 const Utility = require('../utils');
-
-const validateConfig = (api, opts) => {
-  const _opts = Object.assign({}, opts);
-
-  _opts.keyword = validator.isNullOrWhitespace(_opts.keyword) ? 'default' : _opts.keyword;
-
-  _opts.app = typeof (_opts.app) === 'object' ? _opts.app : {};
-
-  _opts.app.developerId = typeof _opts.app.developerId === 'number' ? parseInt(_opts.app.developerId) : undefined;
-
-  _opts.app.defaultLanguage = validator.isNullOrWhitespace(_opts.app.defaultLanguage) ? 'en' : _opts.app.defaultLanguage;
-
-  _opts.app.commandSettings = typeof (_opts.app.commandSettings) === 'object' ? _opts.app.commandSettings : {};
-
-  _opts.app.commandSettings.ignoreOfficialBots = validator.isValidBoolean(_opts.app.commandSettings.ignoreOfficialBots) ? Boolean(_opts.app.commandSettings.ignoreOfficialBots) : false;
-
-  _opts.app.commandSettings.ignoreUnofficialBots = validator.isValidBoolean(_opts.app.commandSettings.ignoreUnofficialBots) ? Boolean(_opts.app.commandSettings.ignoreUnofficialBots) : false;
-
-  _opts.app.networkSettings = typeof (_opts.app.networkSettings) === 'object' ? _opts.app.networkSettings : {};
-
-  _opts.app.networkSettings.retryMode = typeof _opts.app.networkSettings.retryMode === 'number' && Object.values(constants.RetryMode).includes(parseInt(_opts.app.networkSettings.retryMode)) ? parseInt(_opts.app.networkSettings.retryMode) : constants.RetryMode.ALWAYS_RETRY;
-  _opts.app.networkSettings.retryAttempts = typeof _opts.app.networkSettings.retryAttempts === 'number' ? parseInt(_opts.app.networkSettings.retryAttempts) : 1;
-
-  if (_opts.app.networkSettings.retryAttempts <= 0) {
-    console.warn('[WARNING]: minimum retryAttempts is 1');
-    _opts.app.networkSettings.retryAttempts = 1;
-  } else if (_opts.app.networkSettings.retryAttempts >= 4) {
-    console.warn('[WARNING]: maximum retryAttempts is 3');
-    _opts.app.networkSettings.retryAttempts = 3;
-  }
-
-  api._options = {
-    keyword: _opts.keyword,
-    ignoreOfficialBots: _opts.app.commandSettings.ignoreOfficialBots,
-    ignoreUnofficialBots: _opts.app.commandSettings.ignoreUnofficialBots,
-    developerId: _opts.app.developerId,
-    networking: {
-      retryMode: _opts.app.networkSettings.retryMode,
-      retryAttempts: _opts.app.networkSettings.retryAttempts
-    }
-  };
-
-  api._config = _opts;
-};
+const { validateUserConfig } = require('../utils/Config');
+const { Stage } = require('../../typings');
 
 class WOLFBot extends EventEmitter {
   constructor () {
@@ -88,9 +46,9 @@ class WOLFBot extends EventEmitter {
     const configPath = path.join(path.dirname(require.main.filename), '/config/');
 
     if (fs.existsSync(configPath) && fs.existsSync(`${configPath}/default.yaml`)) {
-      validateConfig(this, yaml.parse(fs.readFileSync(`${configPath}/default.yaml`, 'utf-8')));
+      validateUserConfig(this, yaml.parse(fs.readFileSync(`${configPath}/default.yaml`, 'utf-8')));
     } else {
-      validateConfig(this, {});
+      validateUserConfig(this, {});
       console.warn(!fs.existsSync(configPath) ? '[WARNING]: mising config folder\nSee https://github.com/dawalters1/Bot-Template/tree/main/config' : '[WARNING]: missing default.yaml missing in config folder\nSee https://github.com/dawalters1/Bot-Template/blob/main/config/default.yaml');
     }
 
@@ -112,7 +70,7 @@ class WOLFBot extends EventEmitter {
     this._messaging = new Messaging(this);
     this._notification = new Notification(this);
     this._phrase = new Phrase(this);
-    //   this._stage = new Stage(this);
+    this._stage = new Stage(this);
     this._store = new Store(this);
     this._subscriber = new Subscriber(this);
     this._tipping = new Tipping(this);
