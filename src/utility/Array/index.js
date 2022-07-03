0d2ab6389c3b23b66b328c0927a326e4e27b2852
Dave <littledew13099@gmail.com>
2022-07-03 10:26:33 -0400

    utils

--- src/utility/Array/index.js
+++ src/utility/Array/index.js
@@ -1,6 +1,33 @@
+const _ = require('lodash');
 
 class ArrayUtility {
+  chunk (array, length) {
+    return _.chunk(array, length);
+  }
 
+  shuffle (array) {
+    return _.shuffle(array);
+  }
+
+  getRandom (array, amount = 1) {
+    return amount === 1 ? _.sample(array) : _.sampleSize(array, amount);
+  }
+
+  join (arrays) {
+    return _.join(arrays);
+  }
+
+  reverse (array) {
+    return _.reverse(array);
+  }
+
+  take (array, length) {
+    return _.take(array, length);
+  }
+
+  includes (array, object) {
+    return _.includes(array, object);
+  }
 }
 
 module.exports = ArrayUtility;
