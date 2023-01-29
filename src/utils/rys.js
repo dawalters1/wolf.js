977307735239a7c23aec1818aef29e55a5c4b43f
Dave <littledew13099@gmail.com>
2023-01-28 19:23:10 -0500

    enable eslint to rys

--- src/utils/rys.js
+++ src/utils/rys.js
@@ -1,14 +1,12 @@
-/* eslint-disable */
 import path, { dirname } from 'path';
 import fs from 'fs';
 import { Privilege } from '../constants/index.js';
-
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default async  (client, command)=> {
+export default async (client, command) => {
   if (command.sourceSubscriberId === client.currentSubscriber.id) {
     return Promise.resolve();
   }
@@ -21,7 +19,7 @@ export default async  (client, command)=> {
     return Promise.resolve();
   }
 
-  if(!(isDeveloper || (hasProperArgument && await client.utility.subscriber.privilege.has(command.sourceSubscriberId, [Privilege.STAFF, Privilege.VOLUNTEER])) || await client.utility.subscriber.privilege.has(command.sourceSubscriberId, [Privilege.STAFF]))){
+  if (!(isDeveloper || (hasProperArgument && await client.utility.subscriber.privilege.has(command.sourceSubscriberId, [Privilege.STAFF, Privilege.VOLUNTEER])) || await client.utility.subscriber.privilege.has(command.sourceSubscriberId, [Privilege.STAFF]))) {
     return Promise.resolve();
   }
 
@@ -29,12 +27,12 @@ export default async  (client, command)=> {
 
   return await client.messaging.sendMessage(
     command,
-    client.utility.string.replace(client.phrase.getByLanguageAndName(command.language, `${client.config.keyword}_${client._frameworkConfig.get('commandKey')}_with${displayDeveloperDetails?'':'out'}_details_message`),
+    client.utility.string.replace(client.phrase.getByLanguageAndName(command.language, `${client.config.keyword}_${client._frameworkConfig.get('commandKey')}_with${displayDeveloperDetails ? '' : 'out'}_details_message`),
       {
-        version:  JSON.parse(fs.readFileSync(path.join(__dirname, '../../package.json'))).version,
-        nickname: displayDeveloperDetails? (await client.subscriber.getById(client.config.framework.developer)).nickname: '',
-        subscriberId: displayDeveloperDetails? client.config.framework.developer : ''
+        version: JSON.parse(fs.readFileSync(path.join(__dirname, '../../package.json'))).version,
+        nickname: displayDeveloperDetails ? (await client.subscriber.getById(client.config.framework.developer)).nickname : '',
+        subscriberId: displayDeveloperDetails ? client.config.framework.developer : ''
       }
     )
-  )
+  );
 };
