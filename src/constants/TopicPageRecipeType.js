d1075e13a5a13db440218ac21c33541103c2864c
Dave <littledew13099@gmail.com>
2023-04-26 22:38:36 -0400

    add liveEvent support

--- src/constants/TopicPageRecipeType.js
+++ src/constants/TopicPageRecipeType.js
@@ -1,9 +1,11 @@
 export const EVENT = 'event';
+export const LIVE_EVENT = 'liveEvent';
 export const GROUP = 'group';
 export const PRODUCT = 'product';
 
 export default {
   EVENT,
+  LIVE_EVENT,
   GROUP,
   PRODUCT
 };
