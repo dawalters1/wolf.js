91ef5f37ff4884d9de95f1e248b6b83c23e39fa8
dawalters99 <david.walters@palringo.com>
2025-07-23 07:20:55 -0400

    fix exporting of TopicPageRecipeType

--- src/constants/TopicPageRecipeType.js
+++ src/constants/TopicPageRecipeType.js
@@ -1,13 +1,9 @@
-export const CHANNEL = 'group';
-export const EVENT = 'groupEvent';
-export const LIVE_EVENT = 'liveGroupEvent';
-export const PRODUCT = 'product';
-export const USER = 'user';
-
-export default {
-  USER,
-  EVENT,
-  LIVE_EVENT,
-  CHANNEL,
-  PRODUCT
+export const TopicPageRecipeType = {
+  CHANNEL: 'group',
+  EVENT: 'groupEvent',
+  LIVE_EVENT: 'liveGroupEvent',
+  PRODUCT: 'product',
+  USER: 'user'
 };
+
+export default TopicPageRecipeType;
