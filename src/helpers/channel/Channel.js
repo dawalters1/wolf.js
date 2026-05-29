6665d2535b6860c68e5592dc620a217366981c7a
fawazorg <Foazorg@gmail.com>
2026-05-29 18:47:59 +0300

    fix(channel): fetch() passes normalised array to normaliseFetchOpts causing opts object to be treated as IDs
    (cherry picked from commit ffde85208af1278c4c5151c706bdefdab6b95349)

--- src/helpers/channel/Channel.js
+++ src/helpers/channel/Channel.js
@@ -194,7 +194,7 @@ export default class ChannelHelper extends BaseHelper {
 
   async fetch (idsOrName, opts) {
     const normalised = this.normaliseNumbers(idsOrName);
-    const normalisedOpts = this.normaliseFetchOpts(normalised, opts);
+    const normalisedOpts = this.normaliseFetchOpts(idsOrName, opts);
 
     validate(normalisedOpts, this, this.fetch)
       .isNotRequired()
@@ -214,7 +214,7 @@ export default class ChannelHelper extends BaseHelper {
         }
       );
 
-    if (!normalised || this.isObject(normalised)) {
+    if (!idsOrName || this.isObject(idsOrName)) {
       return await this.#fetchChannelList(normalisedOpts);
     }
 
